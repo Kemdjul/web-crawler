1dc5f6c1a57e2554c4d8354583f66b9a4e9cea31
Kemdjul <117106513+Kemdjul@users.noreply.github.com>
2024-04-17 15:53:34 +0200

    Add crawl single page

--- crawl.js
+++ crawl.js
@@ -1,5 +1,28 @@
 const { JSDOM } = require('jsdom');
 
+async function crawlPage(currentURL) {
+    console.log(`actively crawling ${currentURL}`);
+
+    try {
+        const res = await fetch(currentURL);
+        if (res.status > 399) {
+            console.log(`error in fetch with status code: ${res.status} on page: ${currentURL}`);
+            return;
+        }
+
+        const contentType = res.headers.get('content-type');
+        if (!contentType.includes('text/html')) {
+            console.log(`non html response, content type: ${contentType}, on page: ${currentURL}`);
+            return;
+        }
+
+        console.log(await res.text());
+    } catch (err) {
+        console.log(`error in fetch: ${err.message}, on page: ${currentURL}`);
+    }
+
+}
+
 function getURLsfromHTML(htmlBody, baseURL) {
     const urls = [];
     const dom = new JSDOM(htmlBody);
@@ -40,4 +63,5 @@ function normalizeURL(urlString) {
 module.exports = {
     normalizeURL,
     getURLsfromHTML,
+    crawlPage,
 }
